7010e1632fdcc7b9d6074c6495140ada571feeb3
Avadhoot Gururaj <SUGU@EUR.AD.SAG>
2017-10-09 23:47:46 +0530

    Test commit

---
+++ app/src/main/java/com/fliparam/AlarmReceiver.java
@@ -0,0 +1,18 @@
+package com.fliparam;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.design.widget.Snackbar;
+import android.widget.Toast;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        Toast.makeText(context, "A service to start now", Toast.LENGTH_LONG).show();
+
+    }
+}
